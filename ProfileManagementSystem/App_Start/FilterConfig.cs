8f4cca01443ac63009e6ee02548e8ffcc536b470
Shekar <Gnanasekaran.k@cims.co.in>
2020-01-07 19:20:41 +0530

    Initial Check-in...

---
+++ ProfileManagementSystem/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProfileManagementSystem
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
