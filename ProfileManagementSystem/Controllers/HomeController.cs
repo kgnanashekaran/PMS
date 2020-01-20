2168e113eb46ebc9fbc4d6fa93ef5eaddcaa7ade
Shekar <Gnanasekaran.k@cims.co.in>
2020-01-20 22:05:18 +0530

    latest code with scroll

--- ProfileManagementSystem/Controllers/HomeController.cs
+++ ProfileManagementSystem/Controllers/HomeController.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SQLite;
+using System.Data;
 
 namespace ProfileManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataAdapter adapter;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        String connectString;
+
+        public HomeController()
+        {
+
+        }
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
